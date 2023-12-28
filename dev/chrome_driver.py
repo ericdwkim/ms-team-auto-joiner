64ff96e3c9de25126fde49fa27313af169485fa0
ericdwkim <ericdwkim.io@gmail.com>
2023-12-28 14:54:07 -0600

    refactor: `Chrome_Driver` to replace `Driver` class following Singleton pattern with static and class methods

--- dev/chrome_driver.py
+++ dev/chrome_driver.py
@@ -11,38 +11,41 @@ from datetime import datetime
 import json
 from twilio.rest import Client
 
-class Driver:
-    _webdriver = None
+class Chrome_Driver:
+    _chrome_driver_instance = None
 
     def __init__(self):
-        self.setup_driver()
+        self.custom_webdriver = self.setup_web_driver()
         setup_logger()
 
 
-    def setup_driver(self):
-        logging.info('Initializing ChromeDriver with remote debugging')
-        opts = self._get_chrome_options()
-        chromedriver_exec_path = self._get_chromedriver_executable_path()
-
-        self.driver = webdriver.Chrome(
-            service=Service(executable_path=chromedriver_exec_path),
-            options=opts
-        )
-
-
-    def _get_chrome_options(self):
+    @staticmethod
+    def _get_chrome_options():
         opts = webdriver.ChromeOptions()
         opts.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
         return opts
 
-    def _get_chromedriver_executable_path(self):
+    @staticmethod
+    def _get_chromedriver_executable_path():
         return '/opt/homebrew/bin/chromedriver'
 
     @classmethod
-    def get_webdriver(cls):
-        if cls._webdriver is None:
-            cls._webdriver = Driver()
-        return cls._webdriver
+    def get_web_driver_instance(cls):
+        if cls._chrome_driver_instance is None:
+            cls._chrome_driver_instance = Chrome_Driver()
+        return cls._chrome_driver_instance
+
+    def setup_web_driver(self):
+        logging.info('Initializing ChromeDriver with remote debugging')
+        opts = self._get_chrome_options()
+        chromedriver_exec_path = self._get_chromedriver_executable_path()
+
+        web_driver = webdriver.Chrome(
+            service=Service(executable_path=chromedriver_exec_path),
+            options=opts
+        )
+
+        return web_driver
 
 
 
