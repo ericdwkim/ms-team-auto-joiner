64a28b907d5014092ea25216d4143cdbc73b8b89
ericdwkim <ericdwkim.io@gmail.com>
2023-12-28 15:38:44 -0600

    add: `click_on_saved_account` successfully clicks cached user account

--- dev/cached_sign_in_page.py
+++ dev/cached_sign_in_page.py
@@ -1,23 +1,21 @@
 import logging
-from chrome_driver import Chrome_Driver
-import json
+from sign_in_page import SignInPage
 from time import sleep
-import sys
 from utils.log_config import handle_errors
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 
-class CachedSignedInPage:
+class CachedSignedInPage(SignInPage):
 
     def __init__(self):
 
         super().__init__()
 
+    @handle_errors
+    def click_on_saved_account(self):
+        account_selected = self.wait_and_find_element_and_click('//*[@id="tilesHolder"]/div[1]/div/div[1]/div/div[2]/div', By.XPATH)
+        logging.info(f'Click on saved account attempt: {account_selected}')
+        return account_selected  # return the bool whether true or false
 
 
-
-csip = CachedSignedInPage()
-
+account_selected = CachedSignedInPage().click_on_saved_account()
