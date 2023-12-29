d710e95f669ddcacecb44c94c6dd7f087e618d10
ericdwkim <ericdwkim.io@gmail.com>
2023-12-29 17:16:03 -0600

    add: driver, conf, class hierarchy instances in Main constructor - `sign_in` requires testing

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,13 +1,20 @@
 import logging
 import json
+from selenium.webdriver.common.by import By
 from src.app.chrome_driver import Chrome_Driver
 from src.pages.base_page import BasePage
+from src.pages.sign_in_page import SignInPage
+from src.pages.cached_sign_in_page import CachedSignedInPage
 
 
 class Main:
 
     def __init__(self):
+        self.driver = Chrome_Driver().get_web_driver_instance().custom_webdriver
         self.conf = self.load_envs()
+        self.base_page = BasePage(self.driver, self.conf)
+        self.regular_sign_in = SignInPage(self.driver, self.conf)
+        self.cached_sign_in = CachedSignedInPage(self.driver, self.conf)
 
     @staticmethod
     def load_envs():
@@ -15,11 +22,16 @@ class Main:
             config = json.load(f)
             return config
 
+    def sign_in(self):
+        # check to see if cached account element does not exist on DOM
+        if not self.base_page.wait_for_element('//*[@id="tilesHolder"]/div[1]/div/div[1]/div/div[2]/div'):
+            reg_logged_in = self.regular_sign_in.login()
+        cached_logged_in = self.cached_sign_in.login()
+
 
 
 
 if __name__ == "__main__":
-    driver = Chrome_Driver().get_web_driver_instance().custom_webdriver
-    conf = Main().conf
-    page = BasePage(driver, conf)
-    logging.info(page)
+    main_obj = Main()
+    # todo: disable debugger and replace with manual `.get()`
+    main_obj.sign_in()
