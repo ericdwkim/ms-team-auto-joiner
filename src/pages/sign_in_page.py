bbce958c4e314bcbbc126b7bcd59d1423efbeb55
ericdwkim <ericdwkim.io@gmail.com>
2023-12-29 16:56:54 -0600

    chore: cleaned up SignInPage module to inherit from Basepage - requires testing

--- src/pages/sign_in_page.py
+++ src/pages/sign_in_page.py
@@ -1,5 +1,5 @@
 import logging
-from src.app.chrome_driver import Chrome_Driver
+from src.pages.base_page import BasePage
 import json
 from src.utils.log_config import handle_errors
 from selenium.webdriver.common.by import By
@@ -7,68 +7,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class SignInPage:
-    def __init__(self):
-        # @dev: invoke instance method from custom base Driver class and access the pre-configured custom webdriver class attribute via Singleton Pattern
-        self.driver = Chrome_Driver().get_web_driver_instance().custom_webdriver
-        self.conf = self.load_envs()
+class SignInPage(BasePage):
+    def __init__(self, driver, conf):
+        super().__init__(driver, conf)
 
-    @staticmethod
-    def load_envs():
-        with open('/Users/ekim/workspace/personal/MS-Teams-Auto-Joiner/config.json', 'r') as f:
-            config = json.load(f)
-            return config
-    @handle_errors
-    def find_element_and_click(self, locator ,locator_type=By.ID):
-        """
-        Finds element and clicks it using `WebElement.click()`
-        :param locator:
-        :param locator_type:
-        :return: Tuple(bool, WebElement)
-        """
-        element = self.driver.find_element(locator_type, locator)
-        element.click()
-        return (True, element)
 
-    @handle_errors
-    def wait_and_find_element_and_click_and_send_keys(self, locator, keys_to_send):
-        """
-        Find element by locator string, click on element, and send keys
-        :param locator:
-        :param keys_to_send:
-        :return: bool
-        """
-        elem_is_present = self.wait_for_element(locator, locator_type=By.ID)
-        if not elem_is_present:
-            logging.error(f'Tried waiting for element using locator: "{locator}"')
-            return False
-        found_and_clicked, elem = self.find_element_and_click(locator)
-        if found_and_clicked:
-            elem.send_keys(keys_to_send)
-            return True
-        else:
-            logging.error(f'Failed to send keys to element: {locator}')
-            return False
-
-    @handle_errors
-    def wait_for_element(self, locator, locator_type, timeout=15):
-
-        WebDriverWait(self.driver, timeout).until(
-            EC.visibility_of_element_located((locator_type, locator))
-        )
-        return True
-
-    @handle_errors
-    def wait_and_find_element_and_click(self, locator, locator_type):
-
-        elem_is_present = self.wait_for_element(locator, locator_type)
-        if not elem_is_present:
-            logging.error(f'Tried waiting for element using locator: "{locator}"')
-            return False
-        found_and_clicked, elem = self.find_element_and_click(locator, locator_type)
-        if found_and_clicked and elem:
-            elem.click()
-            return True
 
     @handle_errors
     def enter_username(self):
